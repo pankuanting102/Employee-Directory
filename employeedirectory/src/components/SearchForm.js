508f376c7c01c9d984a4bcb1fdc9ee9d246d4757
Kuanting <kuanting@kuan-tings-imac.myfiosgateway.com>
2020-12-16 00:05:13 -0500

    remove search btn

--- employeedirectory/src/components/SearchForm.js
+++ employeedirectory/src/components/SearchForm.js
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { Component }  from "react";
 
-function SearchForm(props) {
-  return (
-    <form>
-      <div className="form-group">
-        
-        <input
-          // onChange={props.handleInputChange}
-          // value={props.search}
-          // name="search"
-          // type="text"
-          // className="form-control"
-          // placeholder="Search for a Gif"
-          // id="search"
-        />
-        <button>
-          Search
-        </button>
-      </div>
-    </form>
-  );
+class SearchForm extends Component {
+
+  handleInputChange = event => {
+    const {name, value} = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    console.log("get succeed")
+    // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
+    this.setState({
+      searchName: "",
+    });
+  };
+  render(){
+    return (
+      <form>
+        <div className="form-group">
+          
+          <input
+            // onChange={props.handleInputChange}
+            // value={props.search}
+            // name="search"
+            // type="text"
+            // className="form-control"
+            // placeholder="Search for a Gif"
+            // id="search"
+            value={this.state.searchName}
+            name="search"
+            onChnage={this.handleInputChange}
+            type="text"
+            placeholder="Search Employee"
+            id="search"
+          />
+          <button onClick={this.handleFormSubmit}>
+            Search
+          </button>
+        </div>
+      </form>
+    );
+  }
+
 }
 
 export default SearchForm;
--- employeedirectory/src/components/SearchResultContainer.js
+++ employeedirectory/src/components/SearchResultContainer.js
@@ -20,12 +20,6 @@ class SearchResultContainer extends Component {
     })
   }
 
-  // searchProfile = query => {
-  //   API.search(query)
-  //     .then(res => this.setState({ results: res.data.data }))
-  //     .catch(err => console.log(err));
-  // };
-
   handleInputChange = event => {
     const name = event.target.name;
     const value = event.target.value;
@@ -34,19 +28,12 @@ class SearchResultContainer extends Component {
     });
   };
 
-  // When the form is submitted, search the Giphy API for `this.state.search`
-  handleFormSubmit = event => {
-    event.preventDefault();
-    this.searchProfile(this.state.search);
-  };
-
   render() {
     return (
       <div>
         <TopNav/>
         <SearchForm
           search={this.state.search}
-          handleFormSubmit={this.handleFormSubmit}
           handleInputChange={this.handleInputChange}
         />
         <ResultList results={this.state.allUsers} />
