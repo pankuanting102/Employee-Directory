035801bf055566e4f27809ebbb197c59fbbde1e4
Kuanting <kuanting@kuan-tings-imac.myfiosgateway.com>
2020-12-19 16:59:36 -0500

    add profile styling

--- employeedirectory/src/components/ResultList.js
+++ employeedirectory/src/components/ResultList.js
@@ -1,20 +1,20 @@
 import React from "react";
-
+import "../styles/ResultList.css"
 function ResultList(props) {
   const profile = props.results
   return (
-    <ul>
+    <div className="wrapper">
     {profile ? profile.map((result, index) => (
-      <div className="list-group-item" key = {index}>
-        <img alt="1" className="img-fluid" src={result.picture.large} />
-        <div>{result.name.first} {result.name.last}</div>
-        <div>{result.phone}</div>
-        <div>{result.email}</div>
-        <div>{result.dob.date.slice(0,10)}</div>
+      <div className="row pd-btm" key = {index}>
+        <img alt="1" className="profile-img table-title col-1" src={result.picture.large} />
+        <div className="table-title col-3">{result.name.first} {result.name.last}</div>
+        <div className="table-title col-3">{result.phone}</div>
+        <div className="table-title col-3">{result.email}</div>
+        <div className="table-title col-2">{result.dob.date.slice(0,10)}</div>
       </div>
       
     )) : "Loaging..."}
-  </ul>
+  </div>
 
 
   );
--- employeedirectory/src/components/TableTitle.js
+++ employeedirectory/src/components/TableTitle.js
@@ -1,16 +1,18 @@
-import React  from 'react';
-import '../styles/TableTitle.css';
-function TableTitle () {
+import React from "react";
+import "../styles/TableTitle.css";
+function TableTitle() {
   return (
-      <div className="container">
-          <div className="table-title">Profile Image</div>
-          <div className="table-title">Name</div>
-          <div className="table-title">Phone Number</div>
-          <div className="table-title">Email</div>
-          <div className="table-title">Date of Birth</div>
-          <hr></hr>
+    <div className="wrapper">
+      <div className="row">
+        <div className="table-title col-1">Image</div>
+        <div className="table-title col-3" >Name</div>
+        <div className="table-title col-3">Phone Number</div>
+        <div className="table-title col-3">Email</div>
+        <div className="table-title col-2">DOB</div>
       </div>
-  )
+      <hr className="rule"></hr>
+    </div>
+  );
 }
 
 export default TableTitle;
